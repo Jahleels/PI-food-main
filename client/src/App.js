129eb511a2d0c4a1f998fd475579fc9f2467b839
Jahleels <jahleels20@gmail.com>
2022-10-09 13:01:26 -0500

    styles and config

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import CreateRecipe from './views/CreateRecipe/CreateRecipe';
+import LandingPage from './views/landingpage/LandingPage';
+import Main from './views/main/Main';
+import RecipeDetail from './views/recipeDetail/RecipeDetail';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Switch>
+        <Route exact path='/'>
+          <LandingPage />
+        </Route>
+        <Route exact path='/home'>
+          <Main />
+        </Route>
+        <Route exact path='/create'>
+          <CreateRecipe />
+        </Route>
+        <Route exact path='/recipe/:id'>
+          <RecipeDetail />
+        </Route>
+      </Switch>
     </div>
   );
 }
